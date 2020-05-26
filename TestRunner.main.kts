cf1d46c71db70a467e18e7a6f0cdf399ab97e9da
Brian Grogan <brian.grogan.jr@gmail.com>
2020-05-25 22:17:09 -0400

    Insert DependsOn and Import declarations

--- TestRunner.main.kts
+++ TestRunner.main.kts
@@ -1,7 +1,17 @@
+@file:DependsOn("junit:junit:4.13")
+@file:DependsOn("org.hamcrest:hamcrest:2.2")
+@file:DependsOn("com.squareup.okhttp3:okhttp:4.7.2")
+@file:DependsOn("com.squareup.moshi:moshi:1.9.2")
+@file:DependsOn("com.squareup.moshi:moshi-kotlin:1.9.2")
+
+@file:Import("NycOpenDataTest.kts")
+@file:Import("OpenStreet.kts")
+@file:Import("NYC.kts")
+
 import org.junit.runner.JUnitCore
 
 val results = JUnitCore.runClasses(
-  ...
+  NycOpenDataTest.OpenStreetTest::class.java
 )
 
 println(
